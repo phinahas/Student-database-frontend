4c2775326545269c2435f0476515d0d7c118b16d
Phinahas Philip <phinahasphilip2000@gmail.com>
2021-06-16 15:39:11 +0530

    Form component for adding new student created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import StudentForm from './components/Add_Student_Form/StudentForm';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <StudentForm/>
+     
     </div>
   );
 }
---
+++ src/components/Add_Student_Form/StudentForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './studentForm.css'
+
+function StudentForm() {
+    return (
+        <div className="main-div">
+
+            <form action="" className="form">
+
+             <h3 className="heading">Add New Student</h3>
+             <input className="inputField" type="text" placeholder="Student Name" value="" /><br />
+             <input className="inputField" type="text" placeholder="Admision Number" value="" /><br />
+             <input className="inputField" type="text" placeholder="Department" value="" /><br />
+             <input className="inputField" type="text" placeholder="Batch(2000-2004)" value="" /><br />
+             <input className="inputField" type="text" placeholder="Mobile Number" value="" /><br />
+             <input className="inputField" type="text" placeholder="Address" value="" /><br />
+
+
+            </form>
+            
+        </div>
+    )
+}
+
+export default StudentForm
