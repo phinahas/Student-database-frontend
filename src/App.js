c33623b77102cff89ae6279ef0a12c4155b76596
Phinahas Philip <phinahasphilip2000@gmail.com>
2021-06-16 15:51:27 +0530

    Component for viewing a particular student made

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import StudentForm from './components/Add_Student_Form/StudentForm';
+import SpecificStudent from './components/Specific_Student/SpecificStudent';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <StudentForm/>
+   
+      <SpecificStudent/>
      
     </div>
   );
---
+++ src/components/Specific_Student/SpecificStudent.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './specificStudent.css'
+
+function SpecificStudent() {
+    return (
+        <div className="main-div">
+            <div className="detailes">
+                <h3>Name:</h3>
+                <p>Admission no:</p>
+                <p>Department:</p>
+                <p>Batch:</p>
+                <p>Address:</p>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default SpecificStudent
