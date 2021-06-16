22afd5617244742acde7280d9c054562f2ad3d33
Phinahas Philip <phinahasphilip2000@gmail.com>
2021-06-16 23:04:43 +0530

    Component for viewing the list of students created.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import StudentForm from './components/Add_Student_Form/StudentForm';
 import SpecificStudent from './components/Specific_Student/SpecificStudent';
+import StudentsList from './components/Students_List/StudentsList';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-   
-      <SpecificStudent/>
+      
+      <StudentsList/>
      
     </div>
   );
---
+++ src/components/Students_List/StudentsList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './studentsList.css'
+function StudentsList() {
+    return (
+        <div className="main-div-student-list">
+            <div className="students-table">
+          <table>
+              <tr>
+                  <th>Name</th>
+                  <th>Admission</th>
+                  <th>View</th>
+                  <th>Delete</th>
+              </tr>
+              <tr>
+                  <td>Phinahas</td>
+                  <td>10958</td>
+                  <td><button style={{backgroundColor:"blue",color:"white"}}>View</button></td>
+                  <td><button style={{backgroundColor:"red", color:"white"}}>Delete</button></td>
+              </tr>
+          </table>
+          </div>
+        </div>
+    )
+}
+
+export default StudentsList
